fa909737f23e07c88a1d74f82119ef77a01c345b
ze <uzzz7899@gmail.com>
2025-04-01 01:37:34 +0900

    첫 inst 화면 구상중중

--- src/components/Inst.jsx
+++ src/components/Inst.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import mainimg from '../assets/img/home.jpg';
 
 const Inst = () => {
   return (
-    <div>Inst</div>
+    <section id='inst'>
+      <img src={mainimg} alt='backgroudImg'/>
+      <div className='inst_wrapper'>
+        <h1 className='inst_title'>
+          웹 개발하는 백지은입니다.
+        </h1>
+        <div className='inst_contant'>
+          <div>변화에 유연하게, </div>
+          <div>새로움의 만남과 활용하는 과정속에
+              끊임없이 발전하는 개발자가 되겠습니다.</div>
+        </div>
+      </div>
+    </section>
   )
 }
 
