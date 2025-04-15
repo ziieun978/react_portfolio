8adc24d5cf78713a4657d79f0821cfb9aced3ee9
ze <uzzz7899@gmail.com>
2025-04-15 12:36:15 +0900

    footer 선완료료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer_wrapper'>
+      <div className='footer_content'>
+        {/* <div className='footer_link'>
+        <a href='깃허브주소'>깃허브주소</a>
+        </div> */}
+        <h4>이 사이트는 리액트를 이용하여 제작된 포트폴리오입니다.</h4>
+
+      </div>
+    </div>
   )
 }
 
