ff3353a06b04da460fa1997519ed3fe2b718ff20
ze <uzzz7899@gmail.com>
2025-04-20 12:50:32 +0900

    contact 가봉봉

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='contact_wrapper'>
+        <div className='contact_text'>
+          <h2>감사합니다</h2>
+        </div>
+        <div className='contact_box'>
+          <div className='contact_box_text'>
+            연락처
+            메일
+            깃허브
+          </div> 
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,3 +6,4 @@ const Nav = () => {
   )
 }
 export default Nav
+// 사용x 교육이나 학력으로 사용할 예정
