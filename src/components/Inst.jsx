c14fcdb7ff86cc165338ccba24075d9ad621a141
ze <uzzz7899@gmail.com>
2025-05-15 00:47:04 +0900

    인트로 수정정

--- src/components/Inst.jsx
+++ src/components/Inst.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 
 const Inst = () => {
   return (
-    <section id='inst'>
-      {/* <img src={mainimg} alt='backgroudImg'/> */}
-      <div className='inst_wrapper'>
-        <h1 className='inst_title'>
-          웹 개발하는 백지은입니다.
+    <section id="inst" className="intro_section">
+      <div className="inst_wrapper">
+        <h1 className="inst_title">
+          사용자 경험을 생각하는 <span className="highlight">백지은</span>입니다.
         </h1>
-        <div className='inst_contant'>
-          <div>변화에 유연하게, </div>
-          <div>새로움의 만남과 활용하는 과정속에
-              끊임없이 발전하는 개발자가 되겠습니다.</div>
+        <div className="inst_content">
+          <div>변화에 유연하게, 화면 속 사용자 경험을 진지하게 고민하는,</div>
+          <div>
+             신입 프론트엔드 개발자 백지은입니다. <br />
+             끊임없이 배우고 성장하며 더 나은 인터페이스를 만들어가고자 합니다.
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
+
 
 export default Inst
