5f3bf80425e13e1e97f3b4100ccd794a7cdf2c8f
ze <uzzz7899@gmail.com>
2025-06-05 23:19:37 +0900

    pro3 modal 완료, card detail 추가

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { projectData } from "../data/ProjectData";
 import ProjectModal from "./ProjectModal";
+import { soloProjects } from "../data/ProjectData";
 
 function ProjectCard() {
   const [selectedProject, setSelectedProject] = useState(null);
 
   return (
-    <div className="projectSection">
+    <section className="projectSection">
       <h2>프로젝트</h2>
       <div className="projectGrid">
         {projectData.map((project) => (
@@ -15,6 +16,9 @@ function ProjectCard() {
             className="projectCard"
             onClick={() => setSelectedProject(project)}
           >
+            {/*  라벨 추가 */}
+            <span className="projectLabel team">팀 프로젝트</span>
+
             <img src={project.thumbnail} alt={project.title} className="projectThumb" />
             <h3>{project.title}</h3>
             <p>{project.shortDesc}</p>
@@ -27,13 +31,38 @@ function ProjectCard() {
         ))}
       </div>
 
+      <h2>개인 프로젝트</h2>
+      <div className="projectGrid">
+        {soloProjects.map((project) => (
+          <div
+            key={project.id}
+            className="projectCard"
+            onClick={() => setSelectedProject(project)}
+          >
+            {/*  라벨 추가 */}
+            <span className="projectLabel solo">개인 학습</span>
+
+            <img src={project.thumbnail} alt={project.title} className="projectThumb" />
+            <h3>{project.title}</h3>
+            <p>{project.shortDesc}</p>
+            <div className="stackIcons">
+              {project.stacks.map((icon, i) => (
+                <img key={i} src={icon} alt={`stack-${i}`} className="stackIcon" />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      
+      
+
       {selectedProject && (
         <ProjectModal
           project={selectedProject}
           onClose={() => setSelectedProject(null)}
         />
       )}
-    </div>
+    </section>
   );
 }
 
